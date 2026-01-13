531c0e001522dacaec3456233e2eb1f6540b54f5
HP_GORAN\goran <goran.anucojic@gmail.com>
2026-01-13 13:43:01 +0100

    Fixed swagger using exception interceptor.

--- src/main/java/com/mds/datacenter/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mds/datacenter/exception/GlobalExceptionHandler.java
@@ -1,17 +1,17 @@
 package com.mds.datacenter.exception;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
